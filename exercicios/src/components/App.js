43907ac5f5225aa80446583e9ccdd4c8b6300b61
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-03-12 09:29:10 -0300

    sistema de modulos do ecmascript

--- exercicios/src/components/App.js
+++ exercicios/src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Primeiro from '../components/Primeiro';
+import {Comp2, Comp3} from '../components/Multi';
 
 const App = function () {
   return (
     <>
       <View>
+        <Comp2 />
+        <Comp3 />
         <Primeiro />
       </View>
-      <Text>Hello World</Text>
     </>
   );
 };
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function Comp1() {
+  return <Text>Comp #01</Text>;
+}
+export function Comp2() {
+  return <Text>Comp #02</Text>;
+}
+export function Comp3() {
+  return <Text>Comp #03</Text>;
+}
