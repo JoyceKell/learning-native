49781d4f01f5e6f9a596bb844d918b2663860ec0
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-03-12 09:13:09 -0300

    organizando componentes

---
+++ exercicios/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Primeiro from '../components/Primeiro';
+
+const App = function () {
+  return (
+    <>
+      <View>
+        <Primeiro />
+      </View>
+      <Text>Hello World</Text>
+    </>
+  );
+};
+
+export default App;
