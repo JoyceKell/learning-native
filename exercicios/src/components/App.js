58ffd5da7b2b7440f568a09a175108d7f1239782
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-03-14 05:28:33 -0300

    componente com estado

--- exercicios/src/components/App.js
+++ exercicios/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import Botao from '../components/Botao';
+import Contador from '../components/Contador';
+//import Botao from '../components/Botao';
 //import Frag from '../components/Frag';
 //import Minmax from '../components/Minmax';
 //import Aleatorio from '../components/Aleatorio';
@@ -10,7 +11,7 @@ import Botao from '../components/Botao';
 const App = function () {
   return (
     <View style={style.App}>
-      <Botao />
+      <Contador inicial={100} passo={2} />
     </View>
   );
 };
---
+++ exercicios/src/components/Contador.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {Text, Button} from 'react-native';
+import Estilo from './estilo';
+
+export default props => {
+  const [numero, setNumero] = useState(props.inicial);
+  const inc = () => {
+    setNumero(numero + props.passo);
+  };
+
+  const dec = () => {
+    setNumero(numero - props.passo);
+  };
+
+  return (
+    <>
+      <Text style={Estilo.grande}>{numero}</Text>
+      <Button title="+" onPress={inc} />
+      <Button title="-" onPress={dec} />
+    </>
+  );
+};
