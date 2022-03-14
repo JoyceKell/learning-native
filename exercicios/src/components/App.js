51fffaff5576df718ae2ef9ca5c9c04dad1da415
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-03-14 01:34:12 -0300

    desafio numero aleatorio

--- exercicios/src/components/App.js
+++ exercicios/src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import Minmax from '../components/Minmax';
+//import Minmax from '../components/Minmax';
+import Aleatorio from '../components/Aleatorio';
 //import Primeiro from '../components/Primeiro';
 //import {Comp2, Comp3} from '../components/Multi';
 
 const App = function () {
   return (
     <View style={style.App}>
-      <Minmax min={3} max={5} />
+      <Aleatorio min={3} max={5} />
     </View>
   );
 };
