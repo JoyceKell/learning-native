933dd5857b6d7fa5b1e9b8ef2d535763f18caa82
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-05-31 09:22:54 -0300

    navegacao drawer

--- navigation/src/navegacao/index.js
+++ navigation/src/navegacao/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import Tab from './Tab';
+import Drawer from './Drawer';
+//import Stack from './Stack';
+//import Tab from './Tab';
 
 export default props => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      <Tab />
+      <Drawer />
+      {/*<Tab /> */}
+      {/*<Stack/> */}
     </NavigationContainer>
   </SafeAreaView>
 );
