f89f5a978e030bbf5f7f13999661f231a802167d
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-06-01 08:03:15 -0300

    finalizando modulo de navegacao

--- navigation/src/navegacao/Tab.js
+++ navigation/src/navegacao/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
@@ -8,6 +9,22 @@ const Tab = createBottomTabNavigator();
 
 export default props => (
   <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list-box' : 'ios-list';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
     tabBarOptions={{
       activeTintColor: 'red',
       inactiveTintColor: 'blue',
