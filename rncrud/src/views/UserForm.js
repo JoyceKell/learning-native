7a07d8754ab53decb2e8ffdc224282eac719b36f
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-06-07 09:20:00 -0300

    usando contextAPI

--- rncrud/src/views/UserForm.js
+++ rncrud/src/views/UserForm.js
@@ -1,6 +1,50 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, StyleSheet, Button} from 'react-native';
 
-export default props => {
-  return <Text>user form</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  return (
+    <View style={style.form}>
+      <Text>Name</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o email"
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe o avatarUrl"
+        value={user.avatarUrl}
+      />
+
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginBottom: 15,
+  },
+});
--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,6 +1,63 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, FlatList, Alert} from 'react-native';
+import {ListItem, Button, Icon} from 'react-native-elements';
+import UsersContext from '../context/UsersContext';
 
 export default props => {
-  return <Text>user list</Text>;
+  const {state} = useContext(UsersContext);
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir usuário?', 'Deseja excluir o usuário?', [
+      {
+        text: 'sim',
+        onPress() {
+          console.warn('delete');
+        },
+      },
+      {
+        text: 'não',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        leftAvatar={{source: {uri: user.avatarUrl}}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        rightElement={getActions(user)}
+        onPress={() => props.navigation.navigate('UserForm', user)}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={state.users}
+        keyExtractor={user => user.id.toString()}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
